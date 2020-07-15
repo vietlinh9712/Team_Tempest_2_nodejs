c14790671c1934ac5fc99297541aff1a8b89da9f
Việt Lê <64905071+vietlinh9712@users.noreply.github.com>
2020-07-15 20:54:35 +0700

    pattern

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, function () {
+app.listen(8080, function () {
     console.log("server is running...");
 });
 app.use(express.static("public"));
